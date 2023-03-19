4f86bda81580496d22fb367b92c04cf20bb6836c
Truong Le <travistrle@gmail.com>
2023-03-18 19:17:32 -0500

    clean up unused page

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,16 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <>
-      <div>Truong Le 2023MAR15 - 215845</div>
-      <NxWelcome title="customer-portal" />
-      <div />
-    </>
-  );
+  return <div>OneGrid.xyz</div>;
 }
 
 export default App;
