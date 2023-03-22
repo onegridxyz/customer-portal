0196352c3ba3317d3cf9d3acf6da245aeab8bf40
Trường Lê <travistrle@gmail.com>
2023-03-21 22:05:05 -0500

    OG-25 add mui (#17)

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,8 +1,17 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
 export function App() {
-  return <div>OneGrid.xyz</div>;
+  return (
+    <div>
+      <span>OneGrid.xyz</span>
+      <div>
+        <Button variant="contained">Hello World</Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
