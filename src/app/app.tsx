e944183a233a4036a4011dfb52929616cf6ff32f
Truong Le <travistrle@gmail.com>
2023-03-22 22:38:05 -0500

    OG-27 add linnk

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import About from 'src/pages/about/About';
 import Home from 'src/pages/home/Home';
 import styles from './app.module.scss';
@@ -7,10 +7,18 @@ import styles from './app.module.scss';
 export function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <div>
+        <span>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/about'}>About</Link>
+        </span>
+      </div>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </div>
   );
 }
